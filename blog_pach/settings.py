3ea5629f79f30bae8602ee975139bebb82370b2f
pachuau10 <duhteapachuau10@gmail.com>
2026-03-11 12:24:18 +0530

    Fix HTTPS proxy for Render

--- blog_pach/settings.py
+++ blog_pach/settings.py
@@ -119,3 +119,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'blog_pach.wsgi.application'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
